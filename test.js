ad31f3e6d4eb8dd84bf5d6c5baf2c3d44b1c303c
Fumihiko-K <132275322+Fumihiko-K@users.noreply.github.com>
2023-05-08 14:15:10 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 for(let number=1;number<=20;number++){
     if(number%3===0 && number%5===0){
-        console.log("FizzBuzz");
+        return"FizzBuzz";
     }else if(number%3===0){
-        console.log("Fizz");
+        return "Fizz";
     }else if(number%5===0){
-        console.log("Buzz");
+        return "Buzz";
     }else{
-        console.log(number);
+        return number;
     }
 }
